c35049e0eb6bde825f1e670d73f30f97fcd9b247
Ashley <adent10@gmail.com>
2022-12-18 17:09:40 +1300

    first commit

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig  = {
+    apiKey: "AIzaSyDvZ_9E5D5pMEJIgmSup_yutHQwunl6lYg",
+    authDomain: "car-database-react-f3fcf.firebaseapp.com",
+    projectId: "car-database-react-f3fcf",
+    storageBucket: "car-database-react-f3fcf.appspot.com",
+    messagingSenderId: "865212495187",
+    appId: "1:865212495187:web:0b25b578a9a93123745da6"
+  };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import { FirebaseAppProvider, AuthCheck } from 'reactfire';
-import { Home, Cars, Contact, About } from './components'  
+import { FirebaseAppProvider, AuthCheck } from 'reactfire';
+import { Home, Cars, Contact, About, SignIn } from './components'  
 import './styles.css'
-// import { firebaseConfig } from './firebaseConfig'
-// import 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig'
+import 'firebase/auth';
 import { Provider } from 'react-redux';
 import { store } from './redux/store'
 
@@ -13,10 +13,11 @@ const temp_props = "Dream Car Sales"
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense={true}> */}
+    <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense={true}>
     <Provider store={store}>
     <Router>
       <Switch>
+
 
 
         <Route exact path="/">
@@ -25,18 +26,21 @@ ReactDOM.render(
         <Route path='/Cars'>
           <Cars></Cars>
         </Route> 
-        <Route path='/contact'>
+        <Route path='/Contact'>
           <Contact></Contact>
         </Route>
-        <Route path='/about'>
+        <Route path='/About'>
           <About></About>
+        </Route>
+        <Route path='/signin'>
+          <SignIn></SignIn>
         </Route>
 
 
       </Switch>
     </Router>
     </Provider>
-    {/* </FirebaseAppProvider> */}
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
